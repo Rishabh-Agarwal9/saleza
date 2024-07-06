c92b8dc4ff1cc33cab3925b2abb3a0229371f9ff
Rishabh-Agarwal9 <rishabha1233@gmail.com>
2024-07-06 17:38:14 +0530

    link fix

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Link from 'next/link';
 import { AiFillGithub} from 'react-icons/ai';
 
 const Footer = () => {
@@ -6,9 +7,14 @@ const Footer = () => {
     <div className="footer-container">
       <p>Made by Rishabh</p>
       <p className="icons">
-      <Link href="https://github.com/Rishabh-Agarwal9/saleza" target="_blank" rel="noopener noreferrer">
-        <AiFillGithub />
-      </Link>
+      <a
+      href="https://github.com/Rishabh-Agarwal9/saleza"
+      target="_blank"
+      rel="noopener noreferrer"
+      style={{ cursor: "pointer" }}
+    >
+      <AiFillGithub />
+    </a>
       </p>
     </div>
   )
